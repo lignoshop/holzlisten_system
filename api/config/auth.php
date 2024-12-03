d834e03bb1a6240992d7a1db69c6cf98aea00d7b
lignoshop <78843153+lignoshop@users.noreply.github.com>
2024-12-03 17:21:17 +0100

    Update auth.php

--- api/config/auth.php
+++ api/config/auth.php
@@ -1,1 +1,21 @@
+<?php
+class Auth {
+    private $secret_key = "your_jwt_secret";
+    
+    public function generateToken($user_id, $username) {
+        $payload = array(
+            "user_id" => $user_id,
+            "username" => $username,
+            "exp" => time() + 3600
+        );
+        return JWT::encode($payload, $this->secret_key);
+    }
 
+    public function validateToken($token) {
+        try {
+            return JWT::decode($token, $this->secret_key, array('HS256'));
+        } catch(Exception $e) {
+            return false;
+        }
+    }
+}
