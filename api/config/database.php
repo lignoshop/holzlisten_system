459134938ffb754732471afae2fbba5a67a268b6
lignoshop <78843153+lignoshop@users.noreply.github.com>
2024-12-03 17:20:53 +0100

    Update database.php

--- api/config/database.php
+++ api/config/database.php
@@ -1,1 +1,19 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "holzlisten_system";
+    private $username = "user";
+    private $password = "password";
+    private $conn;
 
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
