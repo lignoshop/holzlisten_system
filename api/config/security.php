91d2dcd1698e6483cb561f1374e00b2d010671c9
lignoshop <78843153+lignoshop@users.noreply.github.com>
2024-12-03 17:21:36 +0100

    Update security.php

--- api/config/security.php
+++ api/config/security.php
@@ -1,1 +1,16 @@
+<?php
+class Security {
+    public function sanitizeInput($data) {
+        return htmlspecialchars(strip_tags($data));
+    }
 
+    public function validateInput($data, $rules) {
+        $errors = [];
+        foreach($rules as $field => $rule) {
+            if(!isset($data[$field])) {
+                $errors[$field] = "Field required";
+            }
+        }
+        return $errors;
+    }
+}
